57644085e3dc2c65d9ff3e96efaef6e55b45037b
Hamid kadkhoda <hamid.kadkhoda@protonmail.com>
2024-12-27 15:16:29 +0330

    add chart component (using chart js)

---
+++ Components/Chart/Chart.razor.js
@@ -0,0 +1,27 @@
+﻿
+
+function init(identifier) {
+    console.log(identifier);
+
+    const ctx = document.getElementById(`${identifier}`);
+
+
+    new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            datasets: [{
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+}
