93113ef10aaeddde0690c877497a005788026dc6
Hamid kadkhoda <hamid.kadkhoda@protonmail.com>
2025-01-02 00:28:39 +0330

    add messageBox

--- Components/MessageBox/MessageBox.cs
+++ Components/MessageBox/MessageBox.cs
@@ -1,9 +1,19 @@
-﻿namespace ReactiveBlazor;
+﻿using System.Reactive.Linq;
+using System.Reactive.Subjects;
+
+namespace ReactiveBlazor;
 
 public static class MessageBox
 {
+    private static Subject<string> MessageSubject { get; set; } = new Subject<string>();
+
     public static void Show(string message)
     {
+        MessageSubject.OnNext(message);
+    }
 
+    public static IObservable<string> OnChanged()
+    {
+        return MessageSubject.AsObservable();
     }
 }
